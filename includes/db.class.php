0d170e8a819e1a282bc22a1ba2c6871ba9c250bf
Miguelslo27 <miguel.sosa@globant.com>
2018-05-09 23:18:30 -0300

    Debug console

--- includes/db.class.php
+++ includes/db.class.php
@@ -51,7 +51,7 @@
       $this->lastResult = $this->mysqli->query($query) or $this->debugAndDie($query);
 
       $this->consoleLog("[nbQueries]:".$this->nbQueries);
-      $this->consoleLog("[nbQueries]:".json_encode($this->lastResult));
+      $this->consoleLog("[nbQueries]:\"".json_encode($this->lastResult)."\"");
 
 			$this->debug($debug, $query, $this->lastResult);
 			return $this->lastResult;
