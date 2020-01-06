54843f3e8f5437eecdb6a3e5425e1cf9abcb3bda
Miguel <miguelmail2006@gmail.com>
2020-01-05 21:30:18 -0300

    feature/new-home-work-machine - Improved hero message styling

--- templates/components/hero.php
+++ templates/components/hero.php
@@ -3,7 +3,7 @@
 	<div class="hero-message">
 		<h1>Tecnología al alcance de tu mano</h1>
 		<p>Encontrá todos los accesorios para tus gadgets favoritos; fundas, auriculares, powerbanks, y todo lo que se te pueda ocurrir, lo encontrás acá</p>
-		<div>
+		<div class="call-to-action">
 			<a href="#">Empezar a comprar</a>
 		</div>
 	</div>
