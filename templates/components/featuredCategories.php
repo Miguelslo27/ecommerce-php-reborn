f28eada5680fd0c37198b198db739918e3a6a7f5
Miguel <miguelmail2006@gmail.com>
2020-01-08 21:25:24 -0300

    feature/new-home-work-machine - Added featured and new articles look and feel at home

--- templates/components/featuredCategories.php
+++ templates/components/featuredCategories.php
@@ -6,7 +6,39 @@ $categories = getCategories(0, 3);
   <h1>Categorías destacadas</h1>
 
   <?php if (count($categories) == 0) : ?>
-    <p class="is-empty-message">No se encontraron categorías destacadas</p>
+    <!-- <p class="is-empty-message">No se encontraron categorías destacadas</p> -->
+    <ul class="categories">
+      <li>
+        <article>
+          <img src="/statics/images/categories/chargers.jpg" alt="Chargers for mobile">
+          <div class="cat-info">
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+            <a href="#">Lorem ipsum</a>
+            <span>23 artículos</span>
+          </div>
+        </article>
+      </li>
+      <li>
+        <article>
+          <img src="/statics/images/categories/chargers.jpg" alt="Chargers for mobile">
+          <div class="cat-info">
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+            <a href="#">Lorem ipsum</a>
+            <span>23 artículos</span>
+          </div>
+        </article>
+      </li>
+      <li>
+        <article>
+          <img src="/statics/images/categories/chargers.jpg" alt="Chargers for mobile">
+          <div class="cat-info">
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+            <a href="#">Lorem ipsum</a>
+            <span>23 artículos</span>
+          </div>
+        </article>
+      </li>
+    </ul>
   <?php endif; ?>
 
   <?php if (count($categories) > 0) : ?>
@@ -18,7 +50,7 @@ $categories = getCategories(0, 3);
           </div>
           <article>
             <p><?php echo $cat->descripcion_breve; ?></p>
-            <a href="/categorias?c=<?php echo $cat->id; ?>" class="<?php echo ($cat->titulo == $category->titulo) ? 'category-selected' : '' ?>"><?php echo $cat->titulo; ?></a>
+            <a href="/categorias?c=<?php echo $cat->id; ?>"><?php echo $cat->titulo; ?></a>
             <p>23 artículos en esta categoría</p>
           </article>
         </li>
