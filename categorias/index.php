5745847682688688c8a7d9a378845b118052927a
Miguel <miguelmail2006@gmail.com>
2020-01-09 13:11:57 -0300

    feature/admin-category-page - Deleted logs file and fixed some files indentantion

--- categorias/index.php
+++ categorias/index.php
@@ -3,37 +3,34 @@
 $relative = '..';
 require '../includes/common.php';
 
-$userStats = loadUser();
-$appPlace = 'categories';
-$appSubPlace = '';
+$userStats     = loadUser();
+$appPlace      = 'categories';
+$appSubPlace   = '';
 $templatesPath = $GLOBALS['config']['templatesPath'];
 
 startDocument();
 loadSection("header", $userStats);
 
 ?>
-	<section id="body">
-		<div class="body-inner">
+<div class="container categories">
+	<?php
 
+	saveCategory();
+	saveArticle();
+
+	$categories = getCategories(0);
+	$category   = getCategory();
+
+	if (count($category->subcategorias) > 0) {
+		include($templatesPath . 'categories.php');
+	} else {
+		include($templatesPath . 'articles.php');
+	}
+
+	?>
+</div>
 <?php
-
-saveCategory();
-saveArticle();
-
-$categories = getCategories(0);
-$category   = getCategory();
-
-if (count($category->subcategorias) > 0) {
-
-	include($templatesPath . 'categories.php');
-
-} else {
-
-	include($templatesPath . 'articles.php');
-
-}
 
 loadSection("footer", $userStats);
 endDocument();
-
 ?>
