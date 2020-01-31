51ff78d29563e70a9c9ce442bb1638721d088d24
Miguel <miguelmail2006@gmail.com>
2020-01-31 10:35:50 -0300

    feature/404-error-page - Adds styles for 404 page, and files distribution

--- 404/index.php
+++ 404/index.php
@@ -11,43 +11,7 @@ $templatesPath = $GLOBALS['config']['templatesPath'];
 startDocument();
 loadSection("header", $userStats);
 
-?>
-
-<!-- @TODO -->
-<!-- Poner todo esto en una page -->
-<div class="container home">
-  <div class="inner">
-
-    <!-- @TODO -->
-    <!-- Crear estilos para pagina 404 -->
-    <h1 style="font-size: 56px">
-      ¡Ups!
-    </h1>
-    <h1 style="font-size: 56px">
-      Parece que te perdiste...
-    </h1>
-
-    <!-- @TODOs -->
-    <!-- Crear componente de formulario de busqueda -->
-    <!-- Crear estilos propios de este formulario de busqueda -->
-    <form action="/busqueda/" method="GET">
-      <div class="form-line">
-        <label for="clave">Buscar</label>
-        <input type="text" name="clave" class="search-input" placeholder="Qué deseas encontrar?">
-      </div>
-      <div class="form-actions">
-        <button type="submit">Buscar</button>
-        <button type="reset">Regresar</button>
-      </div>
-    </form>
-
-    <?php
-    include($templatesPath . 'components/categories/featuredCategories.php');
-    include($templatesPath . 'components/articles/featuredArticles.php');
-    ?>
-  </div>
-</div>
-<?php
+include($templatesPath . 'pages/404.php');
 
 loadSection("footer", $userStats);
 endDocument();
---
+++ templates/components/forms/search.php
@@ -0,0 +1,8 @@
+<form action="/busqueda/" method="GET">
+  <div class="form-group">
+    <input type="text" name="clave" class="search-input" placeholder="¿Qué deseas encontrar?" autofocus>
+  </div>
+  <div class="form-group">
+    <button type="submit">Buscar</button>
+  </div>
+</form>
