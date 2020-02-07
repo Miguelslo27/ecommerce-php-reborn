98bd9439bef44c93d2aa6373725812166e308dcf
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-02-07 20:25:26 -0300

    bugfix/delete-category-form - Fix the delete category form layout to match look and feel

--- templates/components/admin/forms/delete-category.php
+++ templates/components/admin/forms/delete-category.php
@@ -3,18 +3,16 @@ $category   = getCategory();
 $catid      = isset($_GET['cid']) ? $_GET['cid'] : null;
 ?>
 
-<div class="container category-form">
-	<h2>Eliminando categoria <strong><span class="category-name"></span></strong></h2>
-	<a href="#" class="modal-close action-close"><span class="modal-close-left"></span><span class="modal-close-right"></span></a>
-
-	<h3>Seguro que desea eliminar la categoría <?php echo $category->titulo ?>?</h3>
+<div class="inner form-delete-category">
+	<h1>Estás a punto de eliminar la categoria</h1>
 	<form action="/categorias/" method="POST">
+	  <h3>Seguro que desea eliminar la categoría <?php echo $category->titulo ?>?</h3>
 		<input type="hidden" name="type" value="category">
 		<input type="hidden" name="id" id="id" <?php echo $catid ? 'value="' . $catid . '"' : '' ?>>
 		<input type="hidden" name="delete" id="delete">
-		<div class="form-line form-actions">
-			<button type="submit" class="btn btn-style bnt-login black">Eliminar</button>
-			<button type="button" class="btn btn-style bnt-login grey action-close">Cancelar</button>
+		<div class="form-actions">
+			<button type="submit">Eliminar</button>
+			<button type="reset">Cancelar</button>
 		</div>
 	</form>
 </div>
