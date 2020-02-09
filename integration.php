d5045a15d0b860f70a68193bc7d8c09320e49512
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-02-09 18:39:50 -0300

    demo - Adds continuous integration

--- integration.php
+++ integration.php
@@ -1,6 +1,43 @@
 <?php
 
-foreach ($_REQUEST as $request)
+$level = 0;
+function printRecursively($object)
 {
-  error_log("type:" . $request . "\n", 3, 'logfile.txt');
+  $spaces = array();
+
+  for ($i = 0; $i < $GLOBALS['level']; $i ++) {
+    $spaces[] = '  ';
+  }
+
+  foreach ($object as $key => $value) {
+    if (gettype($value) === 'array' || gettype($value) === 'object') {
+      $GLOBALS['level'] ++;
+      error_log(implode('', $spaces) . $key . " = [" . "\n", 3, 'logfile.txt');
+      printRecursively($value);
+      error_log(implode('', $spaces) . "]" . "\n", 3, 'logfile.txt');
+      $GLOBALS['level'] --;
+    } else {
+      error_log(implode('  ', $spaces) . $key . " = " . $value . "\n", 3, 'logfile.txt');
+    }
+  }
 }
+
+// Chequear si el cliente tiene configurado la integración continua
+// De no ser así, salir del programa
+
+error_log("----------------------------------------------\n", 3, 'logfile.txt');
+error_log("---------------    REQUEST    ----------------\n", 3, 'logfile.txt');
+error_log("----------------------------------------------\n", 3, 'logfile.txt');
+
+printRecursively(apache_request_headers());
+
+error_log("----------------------------------------------\n", 3, 'logfile.txt');
+
+$request = json_decode(file_get_contents("php://input"), true);
+
+printRecursively($request);
+
+exec('git pull', $commandOutput);
+error_log("----------------------------------------------\n", 3, 'logfile.txt');
+
+printRecursively($commandOutput);
