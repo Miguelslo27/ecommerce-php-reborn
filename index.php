047ee5510f01e6e075c65058dd6abb5adfe615f6
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-02-11 10:53:09 -0300

    improvement/refactor-includes - Creates new core functions, replace the home index using the new core

--- index.php
+++ index.php
@@ -1,27 +1,11 @@
 <?php
 
-$relative = './';
-include('./core/common.php');
+$relative = '';
+require_once('core/common.php');
 
-$userStats     = loadUser();
-$page          = 'home';
-$sub_page      = '';
-$template_path = getTemplatePath();
-
-startDocument();
-include($template_path . 'header.php');
-
-?>
-<div class="container home">
-  <?php
-  include($template_path . 'components/hero.php');
-  include($template_path . 'components/categories/featuredCategories.php');
-  include($template_path . 'components/articles/newArticles.php');
-  include($template_path . 'components/articles/featuredArticles.php');
-  ?>
-</div>
-<?php
-
-include($template_path . 'footer.php');
-endDocument();
-?>
+newDocument('home', '', array(
+  'components/hero',
+  'components/categories/featuredCategories',
+  'components/articles/newArticles',
+  'components/articles/featuredArticles'
+));
