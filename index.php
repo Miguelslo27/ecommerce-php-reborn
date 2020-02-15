4840351aed1968a24f3df811e8b1db0f8c9a0d33
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-02-14 21:37:07 -0300

    poc/new-api - Adds dummy recently added articles to home page

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ newDocument([
     'components/header/header',
     'components/lists/categories/featured-categories',
     'components/lists/articles/featured-articles',
+    'components/lists/articles/recently-added-articles',
     'components/footer/footer'
   ],
   'stylesheets' => [
@@ -17,10 +18,10 @@ newDocument([
   'callbefore' => function () {
     $featuredCategories    = getCategories(0, 2);
     $featuredArticles      = getArticles(0, 3);
-    // $recentlyAddedArticles = getArticles(0, 3);
+    $recentlyAddedArticles = getArticles(3, 3);
 
     setGlobal('featuredCategories', oneOf($featuredCategories, []));
     setGlobal('featuredArticles', oneOf($featuredArticles, []));
-    // setGlobal('recentlyAddedArticles', oneOf($recentlyAddedArticles, []));
+    setGlobal('recentlyAddedArticles', oneOf($recentlyAddedArticles, []));
   }
 ]);
