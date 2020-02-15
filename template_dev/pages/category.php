08d1675ca18cd09486dc2f1acb3ca2feea04b0df
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-02-15 20:42:46 -0300

    refactor/components-to-mvc - Adds basics for category detail page

--- template_dev/pages/category.php
+++ template_dev/pages/category.php
@@ -1,1 +1,15 @@
-<h1>Category template</h1>
+<?php
+
+newDocument([
+  'title' => 'eCommerce - Categoria',
+  'page' => 'category',
+  'components' => [
+    'components/header/header',
+    'components/category/category',
+    'components/footer/footer'
+  ],
+  'stylesheets' => [
+    'css/fontawesome/css/all.min.css',
+    'css/layout.css'
+  ]
+]);
