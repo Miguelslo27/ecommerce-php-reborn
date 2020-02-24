4a05fb1b92424ab52f201a63a1cbb2459116b0de
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-02-24 16:30:25 -0300

    refactor/components-to-mvc-cart - Adds cart page and components

--- template_dev/pages/cart.php
+++ template_dev/pages/cart.php
@@ -1,1 +1,20 @@
-<h1>Cart template</h1>
+<?php
+
+newDocument([
+  'title' => 'eCommerce - Mi Carrito',
+  'page' => 'cart',
+  'components' => [
+    'components/header/header',
+    'components/cart/cart',
+    'components/footer/footer'
+  ],
+  'styles' => [
+    'css/fontawesome/css/all.min.css',
+    'css/layout.css',
+    'css/tables.css'
+  ],
+  'beforeRender' => function ()
+  {
+    setGlobal('featuredCategories', getCategories(null, 0, 4));
+  }
+]);
