5be456a14b0ccc7f9c69406c41386cf5508147d2
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-03-03 21:03:23 -0300

    refactor/components-to-mvc-cart - Refactor requires out to its own file

---
+++ core/requires.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once('config.php');
+require_once(CORE_LOCATION . '/lib/class.upload.php');
+require_once(CORE_LOCATION . '/lib/mailer/PHPMailerAutoload.php');
+require_once(CORE_LOCATION . '/api/helpers.php');
+require_once(CORE_LOCATION . '/api/site.php');
+require_once(CORE_LOCATION . '/api/document.php');
+require_once(CORE_LOCATION . '/api/auth.php');
+require_once(CORE_LOCATION . '/api/user.php');
+require_once(CORE_LOCATION . '/api/cart.php');
+require_once(CORE_LOCATION . '/api/category.php');
+require_once(CORE_LOCATION . '/api/article.php');
