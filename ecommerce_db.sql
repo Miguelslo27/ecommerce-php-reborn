7f143538842b48c0ac673318c382ad0c928775a1
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-03-16 18:57:49 -0300

    hotfix/add-to-cart-not-working - Updates database structure and adds some default data to test

--- ecommerce_db.sql
+++ ecommerce_db.sql
@@ -8,7 +8,7 @@
 -- Versión de PHP: 5.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET time_zone = "-03:00";
 
 -- C:\> cmd.exe /c "mysql -u root -p db_name < backup-file.sql"
 -- C:\> cmd.exe /c "mysql -u root -p < e:\ecommerce_db.sql"
@@ -32,22 +32,22 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `articles`;
 CREATE TABLE IF NOT EXISTS `articles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `position` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `code` varchar(20) NOT NULL,
-  `brief_description` tinytext NOT NULL,
-  `description` text NOT NULL,
-  `price` double NOT NULL,
-  `price_offer` double NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `new` tinyint(1) NOT NULL,
-  `spent` tinyint(1) NOT NULL,
-  `offer` tinyint(1) NOT NULL,
-  `images_url` varchar(255) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '1',
-  `_created_at_` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `_updated_at_` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `position` INT NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
+  `code` VARCHAR(20) NOT NULL,
+  `brief_description` TINYTEXT NOT NULL,
+  `description` TEXT NOT NULL,
+  `price` DOUBLE NOT NULL,
+  `price_offer` DOUBLE NOT NULL,
+  `category_id` INT NOT NULL,
+  `new` TINYINT(1) NOT NULL,
+  `spent` TINYINT(1) NOT NULL,
+  `offer` TINYINT(1) NOT NULL,
+  `images_url` VARCHAR(255) NOT NULL,
+  `status` TINYINT(1) NOT NULL DEFAULT '1',
+  `_created_at_` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `_updated_at_` TIMESTAMP NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `name` (`name`,`code`),
   KEY `category_id` (`category_id`),
@@ -63,7 +63,12 @@ TRUNCATE TABLE `articles`;
 -- Volcado de datos para la tabla `articles`
 --
 
--- EMPTY
+INSERT INTO `articles` (`id`, `position`, `name`, `code`, `brief_description`, `description`, `price`, `price_offer`, `category_id`, `new`, `spent`, `offer`, `images_url`, `status`, `_created_at_`, `_updated_at_`) VALUES
+(1, 0, 'hub usb c 5 en 1', 'hubusbc_1', '', '', 89, 0, 1, 1, 0, 0, '/statics/images/articles/1/thumbnail.webp', 1, '2020-03-11 18:03:58', '2020-03-11 18:03:58'),
+(2, 0, 'hub usb c 6 en 1 multipuerto aluminio', 'hubusb_2', '', '', 37, 0, 1, 1, 0, 0, '/statics/images/articles/2/thumbnail.webp', 1, '2020-03-11 18:07:05', '2020-03-11 18:07:05'),
+(3, 0, 'mini llavero grabador atto digital', 'mini01', '', '', 69, 0, 1, 1, 0, 0, '/statics/images/articles/3/thumbnail.webp', 1, '2020-03-11 18:07:05', '2020-03-11 18:07:36'),
+(4, 0, 'tarjeta de sonido usb creative sound blaster x G5', 'tarjsousb001', '', '', 194, 0, 1, 1, 0, 0, '/statics/images/articles/4/thumbnail.webp', 1, '2020-03-11 18:11:20', '2020-03-11 18:11:20'),
+(5, 0, 'tarjeta gráfica dual asus gforce gtx1 1660', 'graph1660', '', '', 194, 0, 1, 1, 0, 0, '/statics/images/articles/5/thumbnail.webp', 1, '2020-03-11 18:11:20', '2020-03-11 18:11:20');
 
 -- --------------------------------------------------------
 
@@ -73,14 +78,14 @@ TRUNCATE TABLE `articles`;
 
 DROP TABLE IF EXISTS `in_order_articles`;
 CREATE TABLE IF NOT EXISTS `in_order_articles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_id` int(11) NOT NULL,
-  `article_id` int(11) NOT NULL,
-  `current_price` double NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `subtotal` double NOT NULL,
-  `_created_at_` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `_updated_at_` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `order_id` INT NOT NULL,
+  `article_id` INT NOT NULL,
+  `current_price` DOUBLE NOT NULL,
+  `quantity` INT NOT NULL,
+  `subtotal` DOUBLE NOT NULL,
+  `_created_at_` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `_updated_at_` TIMESTAMP NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `order_id` (`order_id`,`article_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
@@ -104,16 +109,16 @@ TRUNCATE TABLE `in_order_articles`;
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `position` int(11) NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `brief_description` tinytext NOT NULL,
-  `description` text NOT NULL,
-  `images_url` varchar(255) NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '1',
-  `_created_at_` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `_updated_at_` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `position` INT NOT NULL,
+  `title` VARCHAR(100) NOT NULL,
+  `brief_description` TINYTEXT NOT NULL,
+  `description` TEXT NOT NULL,
+  `images_url` VARCHAR(255) NOT NULL,
+  `category_id` INT NOT NULL,
+  `status` TINYINT(1) NOT NULL DEFAULT '1',
+  `_created_at_` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `_updated_at_` TIMESTAMP NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `title` (`title`),
   KEY `category_id` (`category_id`,`title`)
@@ -128,39 +133,51 @@ TRUNCATE TABLE `categories`;
 -- Volcado de datos para la tabla `categories`
 --
 
--- EMPTY
+INSERT INTO `categories` (`id`, `position`, `title`, `brief_description`, `description`, `images_url`, `category_id`, `status`, `_created_at_`, `_updated_at_`) VALUES
+(1, 0, 'Componentes', '', '', '/statics/images/categories/1/thumbnail.webp', 0, 1, '2020-03-09 18:32:56', '2020-03-11 18:01:33'),
+(2, 0, 'Computadoras', '', '', '', 0, 1, '2020-03-09 18:47:02', '2020-03-09 18:47:02'),
+(3, 0, 'Electrónica', '', '', '', 0, 1, '2020-03-09 18:47:02', '2020-03-09 18:47:02'),
+(4, 0, 'Gammers', '', '', '', 0, 1, '2020-03-09 18:47:32', '2020-03-09 18:47:32'),
+(5, 0, 'Multimedia', '', '', '', 0, 1, '2020-03-09 18:47:32', '2020-03-09 18:47:32'),
+(6, 0, 'Redes y Adaptadores', '', '', '', 0, 1, '2020-03-09 18:48:01', '2020-03-09 18:48:01');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `orders`
+--
 
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE IF NOT EXISTS `orders` (
   -- SUMMARY
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `subtotal` double NOT NULL,
-  `taxes` double NOT NULL, 
-  `discount` double NOT NULL,
-  `total` double NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` VARCHAR(20) NOT NULL,
+  `date` DATETIME NOT NULL,
+  `subtotal` DOUBLE NOT NULL,
+  `taxes` DOUBLE NOT NULL, 
+  `discount` DOUBLE NOT NULL,
+  `total` DOUBLE NOT NULL,
   -- BILLING
-  `billing_name` varchar(100) NOT NULL, 
-  `billing_document` varchar(100) NOT NULL,
-  `billing_address` varchar(100) NOT NULL,
-  `billing_state` varchar(100) NOT NULL,
-  `billing_city` varchar(100) NOT NULL,
-  `billing_zipcode` varchar(100) NOT NULL,
+  `billing_name` VARCHAR(100) NOT NULL, 
+  `billing_document` VARCHAR(100) NOT NULL,
+  `billing_address` VARCHAR(100) NOT NULL,
+  `billing_state` VARCHAR(100) NOT NULL,
+  `billing_city` VARCHAR(100) NOT NULL,
+  `billing_zipcode` VARCHAR(100) NOT NULL,
   -- SHIPPING
-  `shipping_method` tinyint(1) NOT NULL,
-  `shipping_address` varchar(100) NOT NULL,
-  `shipping_state` varchar(100) NOT NULL,
-  `shipping_city` varchar(100) NOT NULL,
-  `shipping_zipcode` varchar(100) NOT NULL,
-  `shipping_agency` varchar(100) NOT NULL,
-  `additional_comments` text NOT NULL,
+  `shipping_method` TINYINT(1) NOT NULL,
+  `shipping_address` VARCHAR(100) NOT NULL,
+  `shipping_state` VARCHAR(100) NOT NULL,
+  `shipping_city` VARCHAR(100) NOT NULL,
+  `shipping_zipcode` VARCHAR(100) NOT NULL,
+  `shipping_agency` VARCHAR(100) NOT NULL,
+  `additional_comments` TEXT NOT NULL,
   -- PAYMENT
-  `payment_method` varchar(100) NOT NULL,
-  `notified` tinyint(1) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '1',
-  `_created_at_` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `_updated_at_` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `payment_method` VARCHAR(100) NOT NULL,
+  `notified` TINYINT(1) NOT NULL,
+  `status` TINYINT(1) NOT NULL DEFAULT '1',
+  `_created_at_` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `_updated_at_` TIMESTAMP NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
@@ -184,10 +201,10 @@ TRUNCATE TABLE `orders`;
 
 DROP TABLE IF EXISTS `subscriptions`;
 CREATE TABLE IF NOT EXISTS `subscriptions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL,
-  `_created_at_` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `_updated_at_` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL,
+  `_created_at_` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `_updated_at_` TIMESTAMP NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `email` (`email`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
@@ -211,22 +228,22 @@ TRUNCATE TABLE `subscriptions`;
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `lastname` varchar(100) NOT NULL,
-  `document` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `verification_code` varchar(100) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  `phone` varchar(100) NOT NULL,
-  `cellphone` varchar(100) NOT NULL,
-  `state` varchar(100) NOT NULL,
-  `city` varchar(100) NOT NULL,
-  `isadmin` tinyint(1) NOT NULL DEFAULT '0',
-  `status` tinyint(1) NOT NULL DEFAULT '1',
-  `_created_at_` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `_updated_at_` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `lastname` VARCHAR(100) NOT NULL,
+  `document` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `verification_code` VARCHAR(100) NOT NULL,
+  `address` VARCHAR(100) NOT NULL,
+  `phone` VARCHAR(100) NOT NULL,
+  `cellphone` VARCHAR(100) NOT NULL,
+  `state` VARCHAR(100) NOT NULL,
+  `city` VARCHAR(100) NOT NULL,
+  `isadmin` TINYINT(1) NOT NULL DEFAULT '0',
+  `status` TINYINT(1) NOT NULL DEFAULT '1',
+  `_created_at_` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `_updated_at_` TIMESTAMP NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   KEY `name` (`name`,`lastname`,`email`)
@@ -241,7 +258,9 @@ TRUNCATE TABLE `users`;
 -- Volcado de datos para la tabla `users`
 --
 
--- EMPTY
+INSERT INTO `users` (`id`, `name`, `lastname`, `document`, `email`, `password`, `verification_code`, `address`, `phone`, `cellphone`, `state`, `city`, `isadmin`, `status`, `_created_at_`, `_updated_at_`) VALUES
+(1, 'Miguel', 'Sosa', '34900078', 'miguelmail2006@gmail.com', 'b6b2c10d1acdfcc71eb332c3a6c7f036', 'ae01ca4477d78b54248bbb2c24c6472a', '39 y J', '', '091 066 416', 'Canelones', 'Parque del Plata', 1, 1, '2020-03-10 19:56:46', '2020-03-11 15:39:48'),
+(2, 'Demo', 'Demo', '000', 'demo@demo.com', '0d0105c8bac42b5655f1773948064a96', '53444f91e698c0c7caa2dbc3bdbf93fc', '', '', '111', '', '', 0, 1, '2020-03-11 18:17:05', '2020-03-15 14:38:37');
 
 -- --------------------------------------------------------
 
