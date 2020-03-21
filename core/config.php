0472940fcc417562af6b4bd2e0683c9bfbb89e57
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-03-21 18:05:46 -0300

    Updates version: Api: 0.5.7->0.5.8

--- core/config.php
+++ core/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('APP_VERSION', '0.8.3');
-define('API_VERSION', '0.5.7');
+define('API_VERSION', '0.5.8');
 
 if (!empty(getenv('ENV'))) {
   require_once('config_' . getenv('ENV') . '.php');
