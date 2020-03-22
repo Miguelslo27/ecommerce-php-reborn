38b84504b2a892e7436ca95e786707c1f9a5f4ce
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-03-22 16:41:43 -0300

    refactor/components-to-mvc-payment - Improves business logic for billing info, adds bussiness logic for shipping info

--- template_dev/pages/payment.php
+++ template_dev/pages/payment.php
@@ -65,6 +65,29 @@ newDocument([
       $billingFullAddress[] = $billingZipcode;
     }
 
+    $shippingInfo        = getOrderShippingInfo(getCurrentCart()->order->id);
+    $shippingAddress     = $shippingInfo->shipping_address;
+    $shippingState       = $shippingInfo->shipping_state;
+    $shippingCity        = $shippingInfo->shipping_city;
+    $shippingZipcode     = $shippingInfo->shipping_zipcode;
+    $shippingFullAddress = array();
+
+    if (!empty($shippingAddress)) {
+      $shippingFullAddress[] = $shippingAddress;
+    }
+
+    if (!empty($shippingState)) {
+      $shippingFullAddress[] = $shippingState;
+    }
+
+    if (!empty($shippingCity)) {
+      $shippingFullAddress[] = $shippingCity;
+    }
+
+    if (!empty($shippingZipcode)) {
+      $shippingFullAddress[] = $shippingZipcode;
+    }
+
     setGlobal('billing_name', $billingName);
     setGlobal('billing_document', $billingDocument);
     setGlobal('billing_fulladdress', implode(', ', $billingFullAddress));
@@ -73,16 +96,65 @@ newDocument([
     setGlobal('billing_city', $billingCity);
     setGlobal('billing_zipcode', $billingZipcode);
     setGlobal('phones', implode(' / ', $phones));
+    setGlobal('shipping_method', $shippingInfo->shipping_method);
+    setGlobal('shipping_fulladdress', implode(', ', $shippingFullAddress));
   }
 ]);
 
-function formHasError($with, $without) {
-  bind(
-    !empty(getSession('request_messages'))
-    && count(getSession('request_messages')->fieldsWithErrors) > 0
-      ? $with
-      : $without
-  );
+function billingInfoFormHasErrors()
+{
+  if (!empty(getSession('request_messages'))) {
+    if (
+      @getSession('request_messages')->fieldsWithErrors['billing_name']
+      || @getSession('request_messages')->fieldsWithErrors['billing_document']
+      || @getSession('request_messages')->fieldsWithErrors['billing_address']
+      || @getSession('request_messages')->fieldsWithErrors['billing_state']
+      || @getSession('request_messages')->fieldsWithErrors['billing_city']
+      || @getSession('request_messages')->fieldsWithErrors['billing_zipcode']
+    ) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function billingInfoIsIncomplete()
+{
+  if (
+    empty(getGlobal('billing_document'))
+    || empty(getGlobal('billing_fulladdress'))
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+function shippingInfoFormHasErrors()
+{
+  if (!empty(getSession('request_messages'))) {
+    if (
+      @getSession('request_messages')->fieldsWithErrors['shipping_address']
+      || @getSession('request_messages')->fieldsWithErrors['shipping_state']
+      || @getSession('request_messages')->fieldsWithErrors['shipping_city']
+      || @getSession('request_messages')->fieldsWithErrors['shipping_agency']
+      || @getSession('request_messages')->fieldsWithErrors['shipping_zipcode']
+    ) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function shippingInfoIsIncomplete()
+{
+  if (empty(getGlobal('shipping_fulladdress'))) {
+    return true;
+  }
+
+  return false;
 }
 
 function fieldHasError($field, $class)
