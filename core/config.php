96cfd68a08617b504fbe53954d7d5bc94eb81558
Miguel Angel Sosa <miguelmail2006@gmail.com>
2020-04-22 20:39:01 -0300

    Update versions: App: 0.10.0->0.10.1 - Api: 0.5.11->0.5.12
    Fix two bugs in the cart
    * Fix an issue when adding negative number updationg items
    * Fix an issue when clicking multiple times in update or remove an item

--- core/config.php
+++ core/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('APP_VERSION', '0.10.0');
-define('API_VERSION', '0.5.11');
+define('APP_VERSION', '0.10.1');
+define('API_VERSION', '0.5.12');
 
 if (!empty(getenv('ENV'))) {
   require_once('config_' . getenv('ENV') . '.php');
