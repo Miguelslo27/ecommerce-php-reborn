c18298042bafdbcb0e9fface311f880e221de5f5
Miguel Angel Sosa <miguelmail2006@gmail.com>
2020-04-23 19:01:55 -0300

    Update Versions: App: 0.10.1->0.10.2 - Api: 0.5.12->0.6.1
    * Adds new loadUser method to the User API
    * Adds new saveUserEdtion method to the User API
    * Fix the user_checkInputData API
    * Fix User edition Form
    * Fix styling issue with the user link in the header

--- core/config.php
+++ core/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('APP_VERSION', '0.10.1');
-define('API_VERSION', '0.5.12');
+define('APP_VERSION', '0.10.2');
+define('API_VERSION', '0.6.1');
 
 if (!empty(getenv('ENV'))) {
   require_once('config_' . getenv('ENV') . '.php');
