f7e7fa843f0302a8480b3029ff06e6f925cc79e7
Miguel Angel Sosa <miguelmail2006@gmail.com>
2020-05-02 20:33:25 -0300

    Update version: Api: 0.6.2->0.6.3

--- core/config.php
+++ core/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('APP_VERSION', '0.10.2');
-define('API_VERSION', '0.6.2');
+define('API_VERSION', '0.6.3');
 
 if (!empty(getenv('ENV'))) {
   require_once('config_' . getenv('ENV') . '.php');
