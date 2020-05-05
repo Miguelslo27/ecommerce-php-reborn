e68c4a9dd4651acdf12221456087be3ef38275ef
Agustina Sosa <agus.smm.2001@gmail.com>
2020-05-04 22:51:57 -0300

    feature/account -  Other filds were added to the form

--- template_dev/components/account/account.php
+++ template_dev/components/account/account.php
@@ -1,7 +1,9 @@
 <body>
   <div class="div-account">
     <section class="info-section">
-      <img class="photo" src="/statics/images/Foto.jpg" alt="FotoPerfil">
+    <div class="photo">
+      <img src="/statics/images/Foto.jpg" alt="FotoPerfil">
+    </div>
       <h2 class= "h2-account">Nombre Apellido</h2>
         <ol>
           <a><ul class="menu"><i class="fas fa-user"></i> | Cuenta <i class="fas fa-angle-right arrow-right"></i></ul></a>
@@ -12,31 +14,34 @@
           <a><ul class="menu"><i class="fas fa-trash-alt"></i> | Eliminar cuenta  <i class="fas fa-angle-right arrow-right"></i></ul></a>
         </ol>
     </section>
+
     <section class="form-section">
       <form action="" class="form-margin">
-        <h2 class="h2-account">Dirección de facturación</h2>
+        <h2 class="shadowed-title">
+          <span class="title-shadow"> Datos de Facturacion</span>
+          <span class="title"> Datos de Facturacion</span>
+        </h2>
         <div class="form-group">
-          <label for="">Nombre</label>
+          <label class="label-center" for="">Nombre</label>
           <input type="text" placeholder="Nombre"> 
-          <label for="">Apellido</label>
+          <label class="label-center" for="">Apellido</label>
           <input type="text" placeholder="Apellido">
         </div>
 
         <div class="form-line">
-          <label for="">Nombre de Empresa</label>
+          <label class="label-center" for="">Nombre de Empresa</label>
           <input type="text" placeholder="Nombre Empresa">
         </div>
 
-        <div class="form-group">
-          <label for="">Direccion</label>
-          <input class="input-space" type="text" placeholder="Calle 1245">
-          <input type="text" placeholder="Apt, Suite, etc">
+        <div class="form-line">
+          <label class="label-center" for="">Direccion Completa</label>
+          <input type="text" placeholder="Calle 1245, esq Dir">
         </div>
 
-        <div class="form-line">
-          <Label>Departamento</Label>
+        <div class="form-group">
+          <Label class="label-center">Departamento</Label>
           <input list="city">
-            <datalist id="city" default="Montevideo">
+            <datalist id="city">
               <option value="Montevideo">
               <option value="Canelones">
               <option value="Lavalleja">
@@ -57,6 +62,17 @@
               <option value="Durazno">
               <option value="Rocha">
             </datalist>
+
+            <label class="label-center" for="">Localidad</label>
+            <input type="text" placeholder="Barrio">
+        </div>
+
+        <div class="form-group">
+          <label class="label-center" for="">Telefono</label>
+          <input type="tel" placeholder="12345678">
+
+          <label class="label-center" for="">Celular</label>
+          <input type="tel" placeholder="12345678">
         </div>
 
         <button class="button primary" id="button-center">Guardar</button>
