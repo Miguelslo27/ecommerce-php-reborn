c839a66fd1a1098261b1d91e019382a2762be2f4
Miguel Angel Sosa <miguelmail2006@gmail.com>
2020-05-23 18:38:54 -0300

    Updates version to test CI

--- core/config.php
+++ core/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('APP_VERSION', '0.10.3');
-define('API_VERSION', '0.7.0');
+define('APP_VERSION', '0.10.3-alpha');
+define('API_VERSION', '0.7.0-alpha');
 
 if (!empty(getenv('ENV'))) {
   require_once('config_' . getenv('ENV') . '.php');
