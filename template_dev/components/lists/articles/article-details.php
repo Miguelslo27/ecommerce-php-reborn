de9c2a72ccc5f60cc0b212dde81878d98b900e09
Federico Sosa <fdsosa.35@gmail.com>
2020-06-02 17:29:43 -0300

    feature/article-details - Added logic and style according to the label

--- template_dev/components/lists/articles/article-details.php
+++ template_dev/components/lists/articles/article-details.php
@@ -1,12 +1,14 @@
 <?php
 $class = '';
 $dscp = '';
+$discount = '';
 if (getGlobal('currentArticle')->new === '1') {
   $class = 'new';
 }
 
 if (getGlobal('currentArticle')->offer === '1') {
   $class = 'offer';
+  $discount = 100 - (((getGlobal('currentArticle')->price_offer) * 100) / (getGlobal('currentArticle')->price));
 }
 
 if (getGlobal('currentArticle')->spent === '1') {
@@ -21,6 +23,7 @@ if (getGlobal('currentArticle')->description === '') {
     <img src="<?php bind(getGlobal('currentArticle')->images_url) ?>" alt="<?php bind(getGlobal('currentArticle')->name) ?>">
   </div>
   <div class="article-info <?php bind($class) ?>">
+    <!--  ARTICLE PATH   -->
     <div class="article-path">
       <a href="/">Inicio / </a>
       <?php if (getGlobal('mainCategory')) : ?>
@@ -28,21 +31,36 @@ if (getGlobal('currentArticle')->description === '') {
       <?php endif ?>
       <a href="/"><?php bind(getGlobal('currentCategory')->title)?></a>
     </div>
+    <!--  TITLE   -->
     <h4><?php bind(getGlobal('currentArticle')->name) ?></h4>
+    <!--  PRICE   -->
     <?php if (getGlobal('currentArticle')->offer === '1') : ?>
       <span class="price before">$<?php bind(getGlobal('currentArticle')->price) ?></span>
-      <span class="price after">$<?php bind(getGlobal('currentArticle')->price_offer) ?></span>
+      <div class="price-discount">
+        <span class="price after">$<?php bind(getGlobal('currentArticle')->price_offer) ?></span>
+        <p><?php bind(round($discount)) ?> % OFF</p>
+      </div>
     <?php else : ?>
       <span class="price">$<?php bind(getGlobal('currentArticle')->price) ?></span>
+    <?php endif ?>
+    <!--  STOCK   -->
+    <?php if ($class != 'spent') : ?>
       <p class="stock">Stock Disponible</p>
+    <?php else : ?>
+      <p class="stock">No hay stock</p>
     <?php endif ?>
     <hr>
-    <div class="actions">
-      <a href="?<?php bind(getQueryParams(['action' => ACTION_ADD_TO_CART, 'aid' => getGlobal('currentArticle')->id])) ?>">Agregar al carrito</a>
-    </div>
+    <!--  CARRITO BUTTON   -->
+    <?php if ($class != 'spent') : ?>
+      <div class="actions">
+        <a href="?<?php bind(getQueryParams(['action' => ACTION_ADD_TO_CART, 'aid' => getGlobal('currentArticle')->id])) ?>">Agregar al carrito</a>
+      </div>
+    <?php endif ?>
+    <!--  DSCP LINK   -->
     <?php if (getGlobal('currentArticle')->description) : ?>
       <a class="link-description" href="#description">>> Ver descripción <<</a>
     <?php endif ?>
+    <!--  ADMIN OPTIONS   -->
     <?php if (isAdmin()) : ?>
       <div class="admin-article-controls">
         <a href="/articulo/editar/?aid=<?php bind(getGlobal('currentArticle')->id) ?>"><i class="far fa-edit"></i></a>
