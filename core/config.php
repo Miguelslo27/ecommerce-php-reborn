573c604a3ca733bdb17e407f31eb3c2db66343c6
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-07-15 16:26:57 -0300

    improvement/sendinblue-integration - Update version to alpha to test integration in dev

--- core/config.php
+++ core/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('APP_VERSION', '0.14.0');
-define('API_VERSION', '0.10.0');
+define('APP_VERSION', '0.14.0-alpha');
+define('API_VERSION', '0.10.0-alpha');
 
 if (!empty(getenv('ENV'))) {
   require_once('config_' . getenv('ENV') . '.php');
