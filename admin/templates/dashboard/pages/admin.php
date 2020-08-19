e87532c82c503a835a7dea55a44add2dc8c52edd
Federico Sosa <fdsosa.35@gmail.com>
2020-08-19 10:38:25 -0300

    feature/admin-dashbord - Created objects using stdClass()

--- admin/templates/dashboard/pages/admin.php
+++ admin/templates/dashboard/pages/admin.php
@@ -18,31 +18,31 @@ newDocument([
   ],
   'beforeRender' => function ()
   {
-    $users         = newStatusObject();
+    $users         = new stdClass();
     $users->title  = 'Usuarios';
     $users->number = '9745';
     $users->links  = ['Todos los usuarios','Usuarios suscriptos','Nuevo usuario','Usuarios suspendidos'];
     $users->icon   = 'fas fa-users';
 
-    $categories         = newStatusObject();
+    $categories         = new stdClass();
     $categories->title  = 'Categorías';
     $categories->number = '9';
     $categories->links  = ['Todos las categorías','Nueva categoría','Categorías eliminadas'];
     $categories->icon   = 'fas fa-th-large';
 
-    $articles         = newStatusObject();
+    $articles         = new stdClass();
     $articles->title  = 'Artículos';
     $articles->number = '94';
     $articles->links  = ['Todos los artículos','Nuevo artículo','Artículos eliminados'];
     $articles->icon   = 'far fa-file';
 
-    $orders         = newStatusObject();
+    $orders         = new stdClass();
     $orders->title  = 'Pedidos totales';
     $orders->number = '706';
     $orders->links  = ['Todos los pedidos','Pedidos pendientes','Pedidos abiertos','Pedidos cerrados','Pedidos cancelados'];
     $orders->icon   = 'fas fa-shopping-cart';
 
-    $conf        = newStatusObject();
+    $conf        = new stdClass();
     $conf->title = 'Configuración';
     $conf->links = ['Datos de la tienda','Información de contacto','Redes sociales','Administradores'];
     $conf->icon  = 'fas fa-cog';
