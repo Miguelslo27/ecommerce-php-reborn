b46e517106a204bfbeeb9e5be1346ce0bd5bff6d
Federico Sosa <fdsosa.35@gmail.com>
2020-09-03 11:07:37 -0300

    feature/admin-nav - Add 'administar' item to the header dropdown when the user is admin

--- template_dev/components/header/user-commands/logged.php
+++ template_dev/components/header/user-commands/logged.php
@@ -1,22 +1,20 @@
 <div class="user-links navigation">
   <div class="dropdown-nav">
-    <a href="/cuenta/" class="access-menu normal-tab">
+    <a href="/cuenta/" class="access-menu normal-tab <?php isAdmin() ? bind('admin-item') : ''?>">
       <i class="fas fa-user-circle"></i>
       <span class="link-label"><?php echo getUserName() ?></span>
     </a>
     <div class="dropdown">
       <a href="/registro" class="access-menu dropdown-item">Edición</a>
+      <?php if(isAdmin()) : ?>
+        <a href="/admin" class="access-menu dropdown-item">Administrar</a>
+      <?php endif ?>
       <a href="/?action=logout" class="access-menu normal-tab">
         <i class="fas fa-sign-out-alt"></i>
         <span class="link-label">Salir</span>
       </a>
     </div>
   </div>
-
-  <?php if (isAdmin()) : ?>
-    <a href="/admin/ordenes" class="access-menu normal-tab">Pedidos</a>
-    <a href="/admin/usuarios" class="access-menu normal-tab">Usuarios</a>
-  <?php endif ?>
 
   <?php if (!isAdmin()) : ?>
     <a href="/carrito" class="access-menu normal-tab">
