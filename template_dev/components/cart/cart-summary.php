a5736747317015bd373562ec6e60742e89a8e5ec
Mike <miguel.angel.y.sosa.menchaca.-nd@disney.com>
2020-10-28 18:10:19 -0300

    feature/taxes - Fixes indentation in cart summary

--- template_dev/components/cart/cart-summary.php
+++ template_dev/components/cart/cart-summary.php
@@ -1,59 +1,47 @@
 <aside class="cart-summary">
-  <h2>Resumen</h2>
+  <h2>resumen</h2>
   <?php
-    $orderSubTotal = number_format(intval(getCurrentCart()->order->total) / 1.22, 2, ',', '');
-    $orderTotal    = number_format(getCurrentCart()->order->total, 2, ',', '');
+  $ordersubtotal = number_format(intval(getcurrentcart()->order->total) / 1.22, 2, ',', '');
+  $ordertotal    = number_format(getcurrentcart()->order->total, 2, ',', '');
   ?>
   <hr>
   <div class="summary resume">
-    <span class="label">Subtotal</span>
-    <span class="value">$ <?php bind($orderTotal) ?></span>
-
+    <span class="label">subtotal</span>
+    <span class="value">$ <?php bind($ordertotal) ?></span>
   </div>
   <hr>
-  <!-- <div class="summary discounts">
-    <span class="label">Descuentos</span>
-    <span class="value">$ 0,00</span>
-  </div>
-  <hr>
-  <div class="summary shipping">
-    <span class="label">Envío</span>
-    <span class="value">$ 100,00</span>
-  </div>
-  <hr> -->
   <div class="summary total">
-    <span class="label">Total</span>
-    <span class="value">$ <?php bind($orderTotal) ?></span>
+    <span class="label">total</span>
+    <span class="value">$ <?php bind($ordertotal) ?></span>
   </div>
   <div class="summary actions">
-
-    <?php if (isLoggedIn()) : ?>
-    <?php if (getGlobal('sub_page') == 'payment') : ?>
-    <a href="/" class="button primary">Continuar</a>
-    <hr>
-    <a href="/carrito" class="button secondary">Editar el carrito</a>
-    <?php endif ?>
-    <?php if (getGlobal('sub_page') == 'shipping') : ?>
-    <a href="/carrito/pagar" class="button primary disabled" id="shipping-button">Continuar
-    </a>
-    <hr>
-    <a href="/categorias" class="button secondary">Seguir comprando</a>
-    <?php endif ?>
-    <?php if (getGlobal('sub_page') == 'billing') : ?>
-    <a href="/carrito/envio" class="button primary disabled" id="billing-button">Continuar
-    </a>
-    <hr>
-    <a href="/categorias" class="button secondary">Seguir comprando</a>
-    <?php endif ?>
-    <?php if (getGlobal('page') == 'cart' && getGlobal('sub_page') == '') : ?>
-    <a href="/carrito/datos-facturacion" class="button primary">Pagar</a>
-    <hr>
-    <a href="/categorias" class="button secondary">Seguir comprando</a>
-    <?php endif ?>
+    <?php if (isloggedin()) : ?>
+      <?php if (getglobal('sub_page') == 'payment') : ?>
+        <a href="/" class="button primary">continuar</a>
+        <hr>
+        <a href="/carrito" class="button secondary">editar el carrito</a>
+      <?php endif ?>
+      <?php if (getglobal('sub_page') == 'shipping') : ?>
+        <a href="/carrito/pagar" class="button primary disabled" id="shipping-button">continuar
+        </a>
+        <hr>
+        <a href="/categorias" class="button secondary">seguir comprando</a>
+      <?php endif ?>
+      <?php if (getglobal('sub_page') == 'billing') : ?>
+        <a href="/carrito/envio" class="button primary disabled" id="billing-button">continuar
+        </a>
+        <hr>
+        <a href="/categorias" class="button secondary">seguir comprando</a>
+      <?php endif ?>
+      <?php if (getglobal('page') == 'cart' && getglobal('sub_page') == '') : ?>
+        <a href="/carrito/datos-facturacion" class="button primary">pagar</a>
+        <hr>
+        <a href="/categorias" class="button secondary">seguir comprando</a>
+      <?php endif ?>
     <?php else : ?>
-    <a href="/login" class="button primary">Ingresar</a>
-    <hr>
-    <a href="/registro" class="button secondary">Registrarme</a>
+      <a href="/login" class="button primary">ingresar</a>
+      <hr>
+      <a href="/registro" class="button secondary">registrarme</a>
     <?php endif ?>
   </div>
 </aside>
