1558c03ab71fa47876b36e00afdac3a687f8b799
Miguel Angel Sosa <miguelmail2006@gmail.com>
2020-11-19 17:16:35 -0300

    Update version: App: 0.21.0a->0.21.1a - Api: 0.15.0a->0.15.1a
    Redirect the user to List of Categories after creating a new one

--- core/config.php
+++ core/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('APP_VERSION', '0.21.0-alpha');
-define('API_VERSION', '0.15.0-alpha');
+define('APP_VERSION', '0.21.1-alpha');
+define('API_VERSION', '0.15.1-alpha');
 
 if (!empty(getenv('ENV'))) {
   require_once('config_' . getenv('ENV') . '.php');
