a05689e1cb8ca888abdbea43fc4c520e192ddf66
Miguel Angel Sosa <miguelmail2006@gmail.com>
2020-11-19 13:24:37 -0300

    Update version: App: 0.20.1a->0.21.0a - Api: 0.14.0a->0.15.0a
    Category CRUD added

--- core/config.php
+++ core/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('APP_VERSION', '0.20.1-alpha');
-define('API_VERSION', '0.14.0-alpha');
+define('APP_VERSION', '0.21.0-alpha');
+define('API_VERSION', '0.15.0-alpha');
 
 if (!empty(getenv('ENV'))) {
   require_once('config_' . getenv('ENV') . '.php');
