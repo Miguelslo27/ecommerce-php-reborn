89d0cef18d4772eb552dc142b29e8359a95ba58a
Agustina Sosa <agus.smm.2001@gmail.com>
2020-12-17 11:04:16 -0300

    feature/list-articles - List articles page

---
+++ admin/templates/pages/list-articles.php
@@ -0,0 +1,17 @@
+<?php
+
+newDocument([
+  'title' => 'eCommerce - Administrar Articulos',
+  'page' => 'admin',
+  'sub_page' => 'articles',
+  'components' => [
+    'components/navbar',
+ ],
+  'styles' => [
+    'components/css/fontawesome/css/all.min.css',
+    'components/css/admin.css',
+    'components/css/forms.css'
+  ],
+  'scripts' => [
+    'components/admin.js'
+  ]
