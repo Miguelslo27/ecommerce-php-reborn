61194dc3fdfce16563cb108fc23bedb32f02411a
Miguel Angel Sosa <miguelmail2006@gmail.com>
2021-02-02 18:35:06 -0300

    Update: App 0.22.6a->0.22.7a - API 0.16.5a->0.16.6a
    List suspended users

--- core/config.php
+++ core/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('APP_VERSION', '0.22.6-alpha');
-define('API_VERSION', '0.16.5-alpha');
+define('APP_VERSION', '0.22.7-alpha');
+define('API_VERSION', '0.16.6-alpha');
 
 if (!empty(getenv('ENV'))) {
   require_once('config_' . getenv('ENV') . '.php');
