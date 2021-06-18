9006eded45d944dfb53e1ae6a9de7b02eb59c9cc
Miguel Angel Sosa <miguelmail2006@gmail.com>
2021-06-18 18:24:21 -0300

    Update: Api: 0.22.13a->0.22.14a / App: 0.16.9a->0.16.10a
    Adds remove button and action to articles list

--- core/config.php
+++ core/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('APP_VERSION', '0.22.13-alpha');
-define('API_VERSION', '0.16.9-alpha');
+define('APP_VERSION', '0.22.14-alpha');
+define('API_VERSION', '0.16.10-alpha');
 
 if (!empty(getenv('ENV'))) {
   require_once('config_' . getenv('ENV') . '.php');
